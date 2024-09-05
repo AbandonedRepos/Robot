c72a4df9b6dca0f7f736d34f0453ad09d7d8f510
CitroenGames <54474809+CitroenGames@users.noreply.github.com>
2024-09-05 21:32:40 +0200

    Update GenerateProject.bat

--- GenerateProject.bat
+++ GenerateProject.bat
@@ -1,2 +1,2 @@
-cmake --preset=default -DCMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows-static -DBUILD_SHARED_LIBS=OFF
+cmake -B Build -DCMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows-static -DBUILD_SHARED_LIBS=OFF
 pause
